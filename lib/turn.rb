98f23956dc08412fe667c847e58f3f132f0d113d
Maria  Morris <mariasemailbox@gmail.com>
2018-05-03 00:53:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -49,7 +49,7 @@ def turn(board)
   index = gets.strip
   user_response = input_to_index(index)
   if valid_move?(board, user_response) == true
-    move (board, user_response)
+    move(board, user_response)
     display_board(board)
 else
   turn(board)
